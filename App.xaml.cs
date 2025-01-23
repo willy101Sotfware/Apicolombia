9393d003d221d4e5bf172df1d00453174b6663ed
Willian <willian@example.com>
2025-01-23 10:23:34 -0500

    Inicialización del proyecto ApiColombia con archivos base

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ApiColombia
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
