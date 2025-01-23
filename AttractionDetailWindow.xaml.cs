f030d0bf4622f2bbb185a16150f844570d9c38f3
Willian <willian@example.com>
2025-01-23 09:45:49 -0500

    Initial commit with .gitignore

---
+++ AttractionDetailWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace ApiColombia
+{
+    public partial class AttractionDetailWindow : Window
+    {
+        public AttractionDetailWindow(List<TouristicAttraction> attractions)
+        {
+            InitializeComponent();
+            AttractionsContainer.ItemsSource = attractions;
+        }
+    }
+}
